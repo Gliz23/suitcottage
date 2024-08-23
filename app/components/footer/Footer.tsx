d809d15e3f97c4339158881a27c7ef69e6e6f246
Your Name <you@example.com>
2024-08-23 15:35:08 +0000

    Project Initialized

---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Container from '../Container'
+
+const Footer = () => {
+  return (
+    <footer className="bg-slate-700
+    text-slate-200 
+    text-sm 
+    mt-16">
+      <Container>
+        <div className="flex flex-col md:flex-row
+        justify-between pt-16 pb-8">
+          
+        </div>
+      </Container>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ app/components/nav/Navbar.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Container from '../Container'
+import Link from 'next/link'
+import { Redressed} from 'next/font/google'
+
+
+const redressed = Redressed({
+  subsets: ['latin'],
+  weight: "400",
+})
+
+const Navbar = () => {
+  
+  return (
+    <div className="
+    sticky
+    top-0
+    w-full
+    bg-slate-200z-30
+    shadow-sm">
+      <div className="py-4 border-b-[1px]">
+        <Container>
+          <div 
+          className="flex items-center justify-between gap-3 md-gap-0">
+            <Link href="/" 
+            className={`${redressed.className}
+                          font-bold text-2xl 
+                          ` }>Suits E-shop </Link>
+            <div className=" hidden md:block ">Search</div>
+            <div className=" flex items-center gap-8 md:gap-12">
+              <div>CartCount</div>
+              <div>User</div>
+            </div>
+          </div>
+        </Container>
+
+      </div> 
+    </div>
+  )
+}
+
+export default Navbar
+
