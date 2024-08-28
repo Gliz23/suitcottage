e731e664e95db709f11bf0254cd7b944f99beda0
Your Name <you@example.com>
2024-08-28 16:51:52 +0000

    Home Banner Component

---
+++ app/components/nav/HomeBanner.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HomeBanner = () => {
+  return (
+    <div className='relatice bg-gradient-to-r from-sky-500 to-sky-700 mb-8'>
+      <div className="mx-auto px-8 py-12 flex flex-col gap-2 md:flex-row items-center">
+        <div>
+            <h1>Summer Sale</h1>
+            <p>Enjoy discounts on selected items</p>
+            <p>Get 50% OFF</p>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default HomeBanner
